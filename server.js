cb763fa891804e5199f06ee493b45161ab197071
Anton Sauh <AntonSauh@Antons-MacBook-Pro.local>
2018-10-29 19:35:51 +0200

    hotfix

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    }
+    // // intercept OPTIONS method
+    // if ('OPTIONS' == req.method) {
+    //     res.send(200);
+    // }
     else {
         next();
     }
@@ -42,7 +42,7 @@ const users = [{
 
 app.get("/users", (req, res, next) => {
     res.status(200);
-res.json(users)
+    res.json(users);
 });
 
 app.post("/login", (req, res, next) => {
