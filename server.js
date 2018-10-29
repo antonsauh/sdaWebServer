69e54a1f76557df3ae87f00eeb09c1f6cdc01c04
Anton Sauh <AntonSauh@Antons-MacBook-Pro.local>
2018-10-29 16:41:44 +0200

    add CORS

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const app = express();
 
 const port = 5000;
 
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json()); // parse form data client
 
