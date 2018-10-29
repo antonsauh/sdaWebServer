ff4a934eef2ca919a6a6f8c68479220bb7369d03
Anton Sauh <AntonSauh@Antons-MacBook-Pro.local>
2018-10-29 19:40:45 +0200

    add key allowance

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const allowCrossDomain = function(req, response, next) {
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, key");
 
     // intercept OPTIONS method
     // if ('OPTIONS' == req.method) {
